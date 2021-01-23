86be924062233dfbe4d7e76ea7c3f7087e79e0a5
Cédric Barreteau <cbarrete@users.noreply.github.com>
2021-01-23 21:43:54 +0100

    Add optional named input/output files

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,17 @@ use screech::read_wav;
 use plotters::prelude::*;
 
 fn main() {
-    let audio_buffer = read_wav(&mut File::open("out.wav").unwrap()).unwrap();
+    let args: Vec<String> = std::env::args().collect();
+    if args.contains(&String::from("-h")) || args.contains(&String::from("--help")) {
+        println!("usage: plotwav {} [input_file [output_file]]", args[0]);
+        return;
+    }
+    let input_file = args.get(1).map(|s| s.as_ref()).unwrap_or("out.wav");
+    let output_file = args.get(2).map(|s| s.as_ref()).unwrap_or("out.png");
 
-    let root = BitMapBackend::new("out.png", (1000, 1000)).into_drawing_area();
+    let audio_buffer = read_wav(&mut File::open(input_file).unwrap()).unwrap();
+
+    let root = BitMapBackend::new(output_file, (1000, 1000)).into_drawing_area();
     root.fill(&WHITE).unwrap();
     let mut chart = ChartBuilder::on(&root)
         .y_label_area_size(40)
